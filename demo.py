d000a702f0869b6e3d71d9d9e4f72efa8bf4c87e
hongyu deng <deng@levitronix.com>
2021-10-06 11:09:46 +0200

    create own Mainwindow, use Widgets and Qt namespace

--- demo.py
+++ demo.py
@@ -5,18 +5,34 @@ Created on Wed Oct  6 10:15:25 2021
 @author: Deng
 """
 
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
+from PyQt5 import QtWidgets
+from PyQt5 import QtCore
+from PyQt5 import QtGui
 #  only needed for access to command line arguments
 import sys
 
-
+#  subclass QMainWindow to customize application's main window
+class MyMainWindow(QtWidgets.QMainWindow):
+    def __init__(self, *arg,**kwargs):
+        super(MyMainWindow, self).__init__(*arg,**kwargs)
+        
+        self.setWindowTitle("My first PyQt5 App")
+        
+        label = QtWidgets.QLabel("what is Qlable?")
+        
+        # the 'Qt' namespace from QtCore has a lot of attributes to customise 
+        # and control widgets, see Http:// doc.qt.io/qt-5/qt.html
+        label.setAlignment(QtCore.Qt.AlignCenter)
+        
+        #set the center wdiget of the window. widget will expand 
+        # to take up all the space in the window by default
+        self.setCentralWidget(label)
+        
 #  you need one and only one QApplication instance per application. it is act as event scheduler
 #  pass in sys.argv to allow command line arguments for your app
-app = QApplication (sys.argv)
+app = QtWidgets.QApplication (sys.argv)
 
-window = QMainWindow()
+window = MyMainWindow()
 window.show()# IMPORTANT !!! windows are hidden by default
 # start the event loop
 app.exec_()
